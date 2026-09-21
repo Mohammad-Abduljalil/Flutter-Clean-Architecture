f0f6ec9a01b42aaf2b99e5151d74d5ec7f26b861
Mohammad Abdaljalil <mhmdabdaljalil2000@gmail.com>
2026-09-21 14:43:26 +0300

    Add User Params

--- lib/features/domain/repository/user_repository.dart
+++ lib/features/domain/repository/user_repository.dart
@@ -1,7 +1,8 @@
 import 'package:clean_architicture/core/errors/failures.dart';
+import 'package:clean_architicture/core/params/user_params.dart';
 import 'package:clean_architicture/features/domain/entities/user_entity.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class UserRepository{
-  Future<Either<Failures,UserEntity>> getUser();
+  Future<Either<Failures,UserEntity>> getUser({required UserParams params});
 }
--- lib/features/domain/usecases/get_user.dart
+++ lib/features/domain/usecases/get_user.dart
@@ -1,4 +1,5 @@
 import 'package:clean_architicture/core/errors/failures.dart';
+import 'package:clean_architicture/core/params/user_params.dart';
 import 'package:clean_architicture/features/domain/entities/user_entity.dart';
 import 'package:clean_architicture/features/domain/repository/user_repository.dart';
 import 'package:dartz/dartz.dart';
@@ -7,7 +8,7 @@ class GetUser{
   final UserRepository repository;
 
   GetUser({required this.repository});
-  Future<Either<Failures,UserEntity>> call(){
-    return repository.getUser();
+  Future<Either<Failures,UserEntity>> call({required UserParams params}){
+    return repository.getUser(params: params);
   }
 }
